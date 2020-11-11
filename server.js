b51c3d8add2d3118370c62108859bc3a60b104dc
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-11-11 16:09:32 -0600

    Working on getting the routes working

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
+app.use(require("./routes/html-routes.js"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethoddb", { useNewUrlParser: true });
 
