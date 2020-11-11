4014195472f389667bfe562ed2da52d6d3a41b4d
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-11-11 17:36:40 -0600

    Trying to get the data to post up

--- routes/api.js
+++ routes/api.js
@@ -11,7 +11,7 @@ router.post("/api/workouts", ({ body }, res) => {
       });
   });
 
-  router.get("/api/workout/range", (req, res) => {
+  router.post("/api/workout/range", (req, res) => {
     Workout.find({})
       .sort({ date: -1 })
       .then(dbWorkout => {
