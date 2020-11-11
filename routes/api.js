81b34af5ce55ac817b2b813661ced11700e2ad0a
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-11-11 17:34:41 -0600

    Attempted to create api calls

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,24 @@
 const router = require("express").Router();
+const Workout = require("../models/workout.js");
 
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
 
-
-
+  router.get("/api/workout/range", (req, res) => {
+    Workout.find({})
+      .sort({ date: -1 })
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
 module.exports = router;
