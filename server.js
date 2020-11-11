d4a6fbf7efe023701e634c1e311fdf2e9fd1818b
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-11-11 15:46:28 -0600

    Fixed the server file so it should work

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-// Add code to userModel.js to complete the model
-
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
-
-const User = require("./userModel.js");
-
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 
@@ -17,29 +12,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethoddb", { useNewUrlParser: true });
 
-// Routes
 
-// Route to post our form submission to mongoDB via mongoose
-app.post("/submit", ({body}, res) => {
-  // Create a new user using req.body
-
-  // Update this route to run the `setFullName` and `lastUpdatedDate` methods before creating a new User
-  // You must create these methods in the model.
-
-  User.create(body)
-    .then(dbUser => {
-      // If saved successfully, send the the new User document to the client
-      res.json(dbUser);
-    })
-    .catch(err => {
-      // If an error occurs, send the error to the client
-      res.json(err);
-    });
-});
-
-// Start the server
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
