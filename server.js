d585a45a7f4bdaeda5df382faa1a4c339901c28b
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-11-11 18:12:05 -0600

    Added quality of life things

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port http://localhost:${PORT}!`);
 });
